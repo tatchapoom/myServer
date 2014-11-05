ae64b0e591996e007afdca233e4634e2cb49ce50
tatchapoom <tatchapoom@hotmail.com>
2014-11-05 19:26:19 +0700

    Change app.js,index.js ย้ายข้อมูลที่แสดง

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,8 +1,7 @@
 angular.module("myApp", [])
-.controller('mainCtrl', function($scope){
-	$scope.books = [
-		{title: 'CSS', price: 500},
-		{title: 'HTML', price: 1300},
-		{title: 'JSON', price: 4000}
-	];
+.controller('mainCtrl', function($scope,$http){
+	$scope.books = [];
+	$http.get('/api/book').success(function(data){
+		$scope.books = data;
+	})
 })
--- index.js
+++ index.js
@@ -3,8 +3,16 @@ var app = express()
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/book', function(req,res){
+	var books = [
+		{title: 'CSS', price: 500},
+		{title: 'HTML', price: 1300},
+		{title: 'JSON', price: 4000}
+ 
+	];
+	res.send(books);
+})
 var server = app.listen(3000, function () {
-
-  	console.log("server is running")
+  console.log('server is running')
 
 })
