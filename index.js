09141372b2fb8861c136e536081c555fca4ab127
tatchapoom <tatchapoom@hotmail.com>
2014-11-12 22:41:43 +0700

    Use mongodb

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var express = require('express')
+var mongojs = require('mongojs')
+var bodyParser = require('body-parser')
 var app = express()
+var db = mongojs('my_server',['book']);
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json())
 
 app.get('/api/book', function(req,res){
-	var books = [
-		{title: 'CSS', price: 500},
-		{title: 'HTML', price: 1300},
-		{title: 'JSON', price: 4000}
- 
-	];
-	res.send(books);
+	db.book.find({}, function(err,books){
+		res.send(books);
+	});
 })
+app.post('/api/book',function(req, res){
+	db.book.insert(req.body, function(err, books){
+		res.send(books);
+	});
+});
+
 var server = app.listen(3000, function () {
   console.log('server is running')
 
